c86da380c0a3579994f01248dd2dbf11beb35728
Kelly Garry <kellygarry@KG-Macbook-Air.local>
2018-08-21 14:57:55 -0600

    ckpt3 assignment completed

---
+++ db/migrations/20180821202023-add-email-to-contacts.js
@@ -0,0 +1,14 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.addColumn(
+      'Contacts',
+      'email',
+      Sequelize.STRING
+    )},
+
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('Contacts', 'email');
+  }
+};
