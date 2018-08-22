76d8d35b8df63fc13927acdc961e490fc5c56d1c
Kelly Garry <kellygarry@KG-Macbook-Air.local>
2018-08-21 21:50:17 -0600

    ckpt4 completed

--- spec/menu_controller_spec.js
+++ spec/menu_controller_spec.js
@@ -1,13 +1,7 @@
 const MenuController = require("../controllers/MenuController");
-// #1
- describe("MenuController", () => {
-   beforeEach(() => {
-      this.menu = new MenuController();
-    });
 
-   describe("#remindMe()", () => {
-     it("should return good advice", () => {
-       expect(this.menu.remindMe()).toBe("Learning is a life-long pursuit");
-     });
-   });
- });
+describe("MenuController", () => {
+  beforeEach(() => {
+    this.menu = new MenuController();
+  });
+});
